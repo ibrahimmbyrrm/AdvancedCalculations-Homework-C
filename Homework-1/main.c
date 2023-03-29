de542fd716974d47ecec047d0a40d36727723409
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-03-29 12:16:01 +0300

    Initial Commit

---
+++ Homework-1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Homework-1
+//
+//  Created by İbrahim Bayram on 29.03.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
